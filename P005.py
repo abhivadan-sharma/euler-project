538b780564256f8251f943ec039f4aedfc8353c2
latebloomer25 <abhivadan25@gmail.com>
2019-12-03 21:55:18 -0500

    Test

--- P005.py
+++ P005.py
@@ -2,16 +2,32 @@
 #
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
+
+# Hint: 2520 is (2 ** 3) * (3 ** 2) * 5 * 7
+
+
 import math
+
 
 def get_all_factors(n):
 	n_max = int(math.sqrt(n)) + 1
 	factors = []
 	for i in range(1, n_max):
-		print(i)
+		# print(i)
 		if n % i == 0:
 			factors.append(i)
 			factors.append(n/i)
 	return factors
 
-get_all_factors(20)
+# get_all_factors(20)
+# get_all_factors(5)
+
+
+n = 20
+ans = 1
+for i in range(2, n+1):
+	if len(get_all_factors(i)) == 2: # checks for prime numbers
+		highest_power = max(x for x in range(1, n+1) if (i ** x) <= n) # highest power which is less than the given number
+		print(i, highest_power)
+		ans = ans * i ** highest_power
+print(ans)
