bc9b3471c6c041aafd6f2cfb9a3b1d5a5b49cb8e
latebloomer25 <abhivadan25@gmail.com>
2019-12-07 22:54:38 -0500

    Test

--- eulerlib.py
+++ eulerlib.py
@@ -6,7 +6,7 @@ def get_primality_upto_n(n):
 	# Sieve of Eratosthenes
 	result = [True] * (n + 1)
 	result[0] = result[1] = False
-	for i in range(math.sqrt(n) + 1):
+	for i in range(0, int(math.sqrt(n)) + 1):
 		if result[i]:
 			for j in range(i * i, len(result), i):
 				result[j] = False
